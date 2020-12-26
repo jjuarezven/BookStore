deb2f5e5258e41da5efa2154e25f89a28cfb1353
Jose G. Juarez R <josejuarez@outlook.com>
2020-12-26 17:54:20 -0500

    Created entities and BookStoreDbContext

--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -1,3 +1,4 @@
+using BookStore.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.API
 {
@@ -24,6 +26,11 @@ namespace BookStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookStoreDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddControllers();
         }
 
---
+++ src/BookStore.Infrastructure/Context/BookStoreDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace BookStore.Infrastructure.Context
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        public BookStoreDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(150)");
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
